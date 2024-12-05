6ba9e67251ff7d2289162e0cbf287eba87177670
siseldyke <siseldyke@gmail.com>
2024-12-05 13:36:04 -0500

    finished module 9

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
-
+from .models import Cat
 # Create your views here.
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse(not needed anymore)
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:(mocked models)
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# # Create a list of Cat instances
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
 
 # Define the home view function
 def home(request):
@@ -28,5 +28,10 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
     # Render the cats/index.html template with the cats data
-    return render(request, 'cats/index.html', {'cats': cats})    
+    return render(request, 'cats/index.html', {'cats': cats}) 
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
